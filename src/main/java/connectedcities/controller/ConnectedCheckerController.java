5c93821dd3874497315f4f31d8145ddbd399e492
Yury Nebieridze <jurius@gmail.com>
2019-02-11 13:15:51 -0500

    class clean up

--- src/main/java/connectedcities/controller/ConnectedCheckerController.java
+++ src/main/java/connectedcities/controller/ConnectedCheckerController.java
@@ -1,33 +1,18 @@
 package connectedcities.controller;
 
-import connectedcities.exceptionhandler.AppException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import connectedcities.service.ISearchService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static connectedcities.controller.Constants.*;
+import static connectedcities.controller.Constants.NO;
+import static connectedcities.controller.Constants.YES;
 
 @Api(value="Connected Cities Application", description="Provides operation for checking connected cities")
 @RestController
